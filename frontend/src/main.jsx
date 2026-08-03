f70ffbda4b492f701cb218c57e5c1b804d9f2962
Eng-AmjedAbdullah <124088388+Eng-AmjedAbdullah@users.noreply.github.com>
2026-08-03 16:40:54 +0000

    Self-host fonts and remove Google Fonts render blocking

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,12 +1,18 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 
+import '@fontsource-variable/noto-kufi-arabic/wght.css'
+import '@fontsource-variable/exo-2/wght.css'
+
 import App from './App.jsx'
 import { LoadingProvider } from './context/LoadingContext'
+
 import './index.css'
 import './preloader-modal-fixes.css'
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(
+  document.getElementById('root')
+).render(
   <React.StrictMode>
     <LoadingProvider>
       <App />
